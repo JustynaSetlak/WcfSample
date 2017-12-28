8618a5e32f23cb8034834b34f2e87c9eda81e137
JustynaSetlak <justa96.s1@gmail.com>
2017-12-28 22:45:55 +0100

    wcf sample

---
+++ WcfSample.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Security;
+using System.Text;
+using System.Threading.Tasks;
+using WcfSample.Client.DataService;
+
+namespace WcfSample.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataServiceClient dataServiceClient = new DataServiceClient();
+            dataServiceClient.ClientCredentials.ServiceCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.None;
+            dataServiceClient.ClientCredentials.UserName.UserName = "test";
+            dataServiceClient.ClientCredentials.UserName.Password = "test123";
+
+            var message = dataServiceClient.GetData();
+            Console.WriteLine(message);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WcfSample/Validators/UserValidator.cs
@@ -0,0 +1,16 @@
+﻿using System.IdentityModel.Selectors;
+using System.Security.Authentication;
+
+namespace WcfSample.Validators
+{
+    public class UserValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (userName != "test" || password != "test123")
+            {
+                throw new AuthenticationException("Incorrect username or password");
+            }
+        }
+    }
+}
